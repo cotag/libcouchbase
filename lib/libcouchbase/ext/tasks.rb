dec21e5789e366107bba244a4d6e23e9c5b52bb4
Stephen von Takach <steve@cotag.me>
2015-06-29 18:48:50 +1000

    add windows build

--- lib/libcouchbase/ext/tasks.rb
+++ lib/libcouchbase/ext/tasks.rb
@@ -6,16 +6,55 @@ file 'ext/libcouchbase/include' do
 end
 
 
+# Check for libuv
+libuv = begin
+    require 'libuv'
+    ::Libuv::Ext.path_to_internal_libuv
+    true
+rescue LoadError, StandardError
+    false
+end
+
+
 if FFI::Platform.windows?
-    
-else # UNIX
+    # -----------
+    # Win32 BUILD
+    # -----------
+
+    arch = FFI::Platform.x64? ? ' Win64' : ''
+
+    file 'ext/libcouchbase/lcb-build' => 'ext/libcouchbase/include' do
+        FileUtils.mkdir('ext/libcouchbase/lcb-build')
+    end
+
+    file "ext/libcouchbase/build/lib/libcouchbase.#{FFI::Platform::LIBSUFFIX}" => 'ext/libcouchbase/lcb-build' do
+        Dir.chdir('ext/libcouchbase/lcb-build') do |path|
+            if libuv
+                system 'cmake', '-with-libuv', ::File.expand_path('../../', ::Libuv::Ext.path_to_internal_libuv), '-G', "Visual Studio 10#{arch}", '..\libcouchbase'
+            else
+                system 'cmake', '-G', "Visual Studio 10#{arch}", '..\libcouchbase'
+            end
+
+            system 'cmake', '--build', '.'
+        end
+    end
+
+else 
+    # -----------
+    # UNIX  BUILD
+    # -----------
+
     file 'ext/libcouchbase/build' => 'ext/libcouchbase/include' do
         FileUtils.mkdir('ext/libcouchbase/build')
     end
 
     file 'ext/libcouchbase/build/makefile' => 'ext/libcouchbase/build' do
         Dir.chdir("ext/libcouchbase") do |path|
-            system "./cmake/configure"
+            if libuv
+                system "./cmake/configure", "-with-libuv", ::File.expand_path('../../', ::Libuv::Ext.path_to_internal_libuv)
+            else
+                system "./cmake/configure"
+            end
         end
     end
 
