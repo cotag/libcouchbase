287bc4e4412a46e06cd0afcd23068233037b0ae6
Stephen von Takach <steve@cotag.me>
2016-10-31 10:53:21 +1100

    (connection) improve hosts option

--- lib/libcouchbase/connection.rb
+++ lib/libcouchbase/connection.rb
@@ -41,7 +41,7 @@ module Libcouchbase
 
         def initialize(hosts: '127.0.0.1', bucket: 'default', password: nil, thread: nil, **opts)
             # build host string http://docs.couchbase.com/sdk-api/couchbase-c-client-2.5.6/group__lcb-init.html
-            hosts = hosts.join(',') if hosts.is_a?(Array)
+            hosts = Array(hosts).flatten.join(',')
             connstr = "couchbase://#{hosts}/#{bucket}"
             connstr = "#{connstr}?#{opts.map { |k, v| "#{k}=#{v}" }.join('&') }" unless opts.empty?
 
