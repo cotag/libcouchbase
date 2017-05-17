e66630e955c93fbd94180bbb3d74b078a7bd0e00
Stephen von Takach <steve@cotag.me>
2017-05-17 19:58:04 +0930

    (tasks:build ext) raise error if build fails on installation
    fixes #2

--- lib/libcouchbase/ext/tasks.rb
+++ lib/libcouchbase/ext/tasks.rb
@@ -74,14 +74,18 @@ else
     end
 
     file 'ext/libcouchbase/build/makefile' => 'ext/libcouchbase/build' do
+        result = nil
         Dir.chdir("ext/libcouchbase") do |path|
-            system './cmake/configure', '-with-libuv', ::File.expand_path('../../', ::Libuv::Ext.path_to_internal_libuv)
+            result = system './cmake/configure', '-with-libuv', ::File.expand_path('../../', ::Libuv::Ext.path_to_internal_libuv)
         end
+        raise 'could not find cmake on path' unless result
     end
 
     file "ext/libcouchbase/build/lib/libcouchbase_libuv.#{FFI::Platform::LIBSUFFIX}" => 'ext/libcouchbase/build/makefile' do
+        result = nil
         Dir.chdir('ext/libcouchbase/build') do |path|
-            system 'make'
+            result = system 'make'
         end
+        raise 'make failed' unless result
     end
 end
